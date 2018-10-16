e02a0abe33c999c3f49b8710fb0ba2407c75b450
sendsms <wj277104307@hotmail.com>
2018-10-16 20:23:52 +0800

    merge with master

--- src/predict.py
+++ src/predict.py
@@ -32,15 +32,30 @@ def predict(model, preprocessing=False):
 
     with open(os.path.join(results_folder, 'matching.pkl'), 'wb') as f:
         pickle.dump(matching, f)
-
+    
+    matching_list = []
     with open(os.path.join(results_folder, 'result.csv'), 'w') as f:
         csvwriter = csv.writer(f)
         for pro, value in sorted(matching.items()):
             top_10 = sorted(value, reverse=True)[:10]
             top_10_ligents = list(map(lambda x: x[1], top_10))
             row = [pro + ", " + ", ".join(top_10_ligents)]
+            matching_list.append(row)
             csvwriter.writerow(row)
 
+    def cal_success_rate(matching_list=matching_list):
+        success_count = 0
+        failure_count = 0
+        for item in matching_list:
+            protein_indice = item[0] 
+            ligend_indices = item[1:]
+            if protein_indice in ligend_indices:
+                success_count += 1
+            else:
+                failure_count += 1
+        return success_count / (success_count + failure_count)
+
+    print(cal_success_rate())
 
 if __name__ == '__main__':
-    predict('./CS5242Project/results/2018-10-13_14:08:35.559978+08:00/model.h5')
+    predict('./results/2018-10-15_15:28:21.109066+08:00/model.h5')

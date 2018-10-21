86a5ff84f92eca0041db8b0ef8f480b9b49c2b4f
sendsms <wj277104307@hotmail.com>
2018-10-21 11:23:36 +0800

    Some working plot history script. Small issue to be fixed: results folder structure.

--- src/plot_training.py
+++ src/plot_training.py
@@ -1,30 +1,43 @@
 import paramiko
+import os
 import getpass
 import pickle
 import matplotlib.pyplot as plt
+from settings import results_folder
 
 
 def download_file():
     hostname = 'nus.nscc.sg'
     username = input("Input your student id: ")
+    password = getpass.getpass(prompt='Enter your password: ')
+    username = 'e0319586'
+    password = 'Haha39462070*'
     basedir = '/home/users/nus/' + str(username) + '/'
-    localfile = './results/historys/history.pickle'
+    localdir = './results/historys/'
+    localfiles = []
+
+    if not os.path.exists(localdir):
+        os.makedirs(localdir)
 
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname=hostname, username=username, password='')
+    ssh.connect(hostname=hostname, username=username, password=password)
 
-    input, output, error = ssh.exec_command('ls ' + basedir + '*/results/*/history.pickle')
+    In, Out, Err = ssh.exec_command('ls ' + basedir + '*/results/*history.pickle')
 
-    for line in output:
-        print(line)
-    #ftp_client = ssh.open_sftp()
-    #ftp_client.get(remotefile, localfile)
-    #ftp_client.close()
+    ftp_client = ssh.open_sftp()
+    for remotefile in Out:
+        remotefile = remotefile.rstrip('\r|\n')
+        localfile = os.path.join(localdir, remotefile.split('/')[-1])
+        localfiles.append(localfile)
+        ftp_client.get(remotefile, localfile)
+    ftp_client.close()
 
     ssh.close()
+    
+    return localfiles
 
-def plot_scores(file='./results/history.pickle'):
+def plot_scores(file=''):
     with open(file, 'rb') as f:
         data = pickle.load(f)
 
@@ -41,4 +54,9 @@ def plot_scores(file='./results/history.pickle'):
     plt.show()
 
 if __name__ == '__main__':
-    plot_scores()
+    history_files = download_file()
+    for index, file in enumerate(history_files):
+        print(f'#{index} :- {file}')
+    print('Choose which history you want to plot: ')
+    choice = input('Choice: ')
+    plot_scores(history_files[int(choice)])
